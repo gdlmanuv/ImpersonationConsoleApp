2021ee8faf44a323715ea5f000b1ac646fca8cb3
Manuel Valle <manuel.valle@flex.com>
2021-06-03 13:32:23 -0500

    NLog implementation completed

--- ImpersonationConsoleApp/Program.cs
+++ ImpersonationConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ImpersonationConsoleApp.Core;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +11,48 @@ namespace ImpersonationConsoleApp
 {
     class Program
     {
-        private const string USER_NAME = "";
-        private const string PASSWORD = "";
+        private const string USER_NAME = "sac_fa_ffown";
+        private const string PASSWORD = "6H#$9re2W$$W#__wW_#r";
         private const string DOMAIN = "AMERICAS";
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         static void Main(string[] args)
         {
-            Task[] tasks =
-            {
-                Task.Run(() =>
-                {
-                    using (Impersonator impersonator = new Impersonator(USER_NAME, PASSWORD, DOMAIN))
-                    {
+            DateTime impersonationStarted;
+            DateTime impersonationEnded;
+            TimeSpan diff;
 
+            try
+            {
+                if (int.TryParse(ConfigurationManager.AppSettings["totalImpersonationsByExecution"], out int totalImpersonations))
+                {
+                    for (int i = 0; i < totalImpersonations; i++)
+                    {
+                        impersonationStarted = DateTime.Now;
+
+                        using (Impersonator impersonator = new Impersonator(USER_NAME, DOMAIN, PASSWORD))
+                        {
+                            impersonationEnded = DateTime.Now;
+                        }
+
+                        diff = impersonationEnded - impersonationStarted;
+
+                        Logger.Info(string.Concat(
+                            $"Impersonation started at: {impersonationStarted.ToString("hh:mm:ss.fff")}, ",
+                            $"ended at: {impersonationEnded.ToString("hh:mm:ss.fff")}. ",
+                            $"Process took {Math.Round(diff.TotalMilliseconds)} miliseconds."));
                     }
-                }),
-            };
+                }
+                else
+                {
+                    throw new Exception("Invalid setting value for totalImpersonationsByExecution");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
         }
     }
 }
